d23e140f3372deae2dad3c1ca0ce4d48dca7206d
Andrey Silin <andronikus@yandex.ru>
2024-01-10 11:14:16 +0300

    correction to comment #7

--- brain_games/games/prime.py
+++ brain_games/games/prime.py
@@ -1,7 +1,21 @@
 from random import randint
-from brain_games.games.gcd import get_multipliers
 
 PRE_QUESTION = 'Answer "yes" if given number is prime. Otherwise answer "no".'
+
+
+def get_multipliers(number):
+    multipliers = []
+    max_multiplier = int(number ** (1 / 2))
+    multiplier = 2
+    while multiplier <= max_multiplier:
+        if number % multiplier == 0:
+            multipliers.append(multiplier)
+            number = number / multiplier
+        else:
+            multiplier += 1
+    if number != 1:
+        multipliers.append(int(number))
+    return multipliers
 
 
 def solution():
