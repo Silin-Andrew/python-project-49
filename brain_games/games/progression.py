8fdbf6cd8d2d1ed753793f9bb973b33e56333cb6
Andrey Silin <andronikus@yandex.ru>
2024-01-23 12:40:37 +0300

    corretion to coment #17

--- brain_games/games/progression.py
+++ brain_games/games/progression.py
@@ -9,20 +9,18 @@ PROGRESSION_LENGTH = 10
 
 
 def get_progression(initial_term, common_difference, length):
-    progression = [initial_term]
+    progression = [str(initial_term)]
     index = 1
     while index <= length - 1:
         initial_term += common_difference
-        progression.append(initial_term)
+        progression.append(str(initial_term))
         index += 1
     return progression
 
 
 def get_question(progression, hidden_index):
     progression[hidden_index] = '..'
-    question = ''
-    for element in progression:
-        question += str(element) + ' '
+    question = ' '.join(progression)
     return question
 
 
@@ -32,6 +30,6 @@ def get_question_and_correct_answer():
     progression = get_progression(initial_term, common_difference,
                                   PROGRESSION_LENGTH)
     hidden_index = randint(0, PROGRESSION_LENGTH - 1)
-    correct_answer = str(progression[hidden_index])
+    correct_answer = progression[hidden_index]
     question = get_question(progression, hidden_index)
     return question, correct_answer
